8fb2cc44bc84c656e033e88226ea433ad6e68350
Arphen Lin <arphenlin@gmail.com>
2016-02-18 09:38:31 +0800

    Update utility.js

--- user_script_libs/utility.js
+++ user_script_libs/utility.js
@@ -1,13 +1,17 @@
+/* Usage: 
+ *  myLog.init('TW116'); // before you use myLog, set the title of your script. 
+ *  myLog.log('hello world!'); // print any message you want, then it will display in chrome console.
+ */
 var myLog = {
-    label: 'YOUR_LABEL',
-    init: function(label) {
-        myLog.label = label;
+    title: 'YOUR_SCRIPT',
+    init: function(title) {
+        myLog.title = title;
     },
     log: function(text) {
         try {
             var d = new Date();
             var n = d.toLocaleString();
-            console.log(n + ' [' + myLog.label + '] ' + text);
+            console.log(n + ' [' + myLog.title + '] ' + text);
         } catch (err) {}
     }
 };
