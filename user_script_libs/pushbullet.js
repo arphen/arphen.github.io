2eaa80b2cb0c8508e2d1ffda3453f29180aa83be
Arphen Lin <31086+arphen@users.noreply.github.com>
2020-04-27 20:01:44 +0800

    Update pushbullet.js

--- user_script_libs/pushbullet.js
+++ user_script_libs/pushbullet.js
@@ -1,11 +1,12 @@
 /**
  * pushbullet.js
- * version: 20200426v3
+ * version: 20200427
  */
 
 /**
  * 使用Pushbullet API發送訊息，參考 - https://docs.pushbullet.com/#push
  * API - https://api.pushbullet.com/v2/pushes
+ * 免費帳號每月可發送500則 - https://docs.pushbullet.com/#ratelimiting
  * @param {*} token 從這裡取得 https://www.pushbullet.com/#settings/account
  * @param {*} title
  * @param {*} body
