0ba416caf30133f096014a629d52c386add67391
Arphen Lin <31086+arphen@users.noreply.github.com>
2023-04-05 19:46:16 +0800

    ok

--- demo_app/demo.js
+++ demo_app/demo.js
@@ -1,6 +1,31 @@
-
-function main() {
-   alert('hello app');
-}
-
-setTimeout(main, 3000);
+function init() {
+    $("#logout_icon").on("click", function() {
+      alert('clicked');
+      toggleMenu();
+    });
+  }
+  
+  function toggleMenu() {
+    menuOnOff();
+    maskOnOff();
+  }
+  
+  function maskOnOff() {
+    if ($('#mask').css('visibility') == 'hidden') {
+      $('#mask').css('visibility', 'visible');
+      $('#mask').css('z-index', '100');
+    } else {
+      $('#mask').css('visibility', 'hidden');
+      $('#mask').css('z-index', '0');
+    }
+  }
+  
+  function menuOnOff() {
+    if ($('#menu').css('visibility') == 'hidden') {
+      $('#menu').css('visibility', 'visible');
+      $('#menu').css('z-index', '101');
+    } else {
+      $('#menu').css('visibility', 'hidden');
+      $('#menu').css('z-index', '0');
+    }
+  }
